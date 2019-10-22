c0d65c6519e2771919fe63d661de67a925e4711a
ussy <sudo5in5k@gmail.com>
2019-10-22 16:59:18 +0900

    add onehot, binning, write csv, memory tips

--- pre_process.py
+++ pre_process.py
@@ -12,83 +12,80 @@ import pandas as pd
 if __name__ == '__main__':
     df = pd.read_csv("./dataset/c01.csv", encoding='cp932')
     column_list = list(df.columns)
-    # print(column_list)  # columnを取得する
+    print(column_list)  # columnを取得する
 
     df_s = df.sort_values("都道府県コード")  # ソート
-    # print(type(df_s.columns))
+    print(type(df_s.columns))
 
     """インデックス"""
     index = df_s.index  # インデックス
     df_s = df_s.reset_index(drop=True)  # Falseでindexが中に含まれる
-    # print(df_s.index)
-
-    # print(df_s.head)  # 最初だけ表示する、重いデータに対してよく使う
+    print(df_s.index)
+    print(df_s.head)  # 最初だけ表示する、重いデータに対してよく使う
 
     """値抽出の仕方"""
-    # print(type(df_s.values[0]))
+    print(type(df_s.values[0]))
 
     prefecture_code_series = df_s['都道府県コード']
-    # print(prefecture_code_series)  # 直接column nameで指定してseries型で返す
+    print(prefecture_code_series)  # 直接column nameで指定してseries型で返す
 
-    # print(df_s.values[0][1])
-    # print(prefecture_code_series[0])
+    print(df_s.values[0][1])
+    print(prefecture_code_series[0])
 
     # at, iat: 単独の要素の値
-    # print(df_s.at[0, r"都道府県コード"])  # index & column name
-    # print(df_s.iat[0, 1])  # num and num
+    print(df_s.at[0, r"都道府県コード"])  # index & column name
+    print(df_s.iat[0, 1])  # num and num
 
     # loc, iloc: 単独及び複数の要素の値
     # speed: at iat > loc, iloc
-    # print(df_s.loc["0":"1", ::4])  # slice
-    # print(df_s.loc[::, "都道府県コード":"元号"])  # slice
+    print(df_s.loc["0":"1", ::4])  # slice
+    print(df_s.loc[::, "都道府県コード":"元号"])  # slice
     """"""
 
     """欠損値の扱い"""
     # 削除
     df_d = df_s.dropna(how='all', axis=1)
-    # print(df_d)
+    print(df_d)
 
-    # print(df_s.dropna(subset=['注']))
+    print(df_s.dropna(subset=['注']))
 
     # 一括で置き換え
     df_r = df_s.replace('-', np.nan)
-    # print(df_r)
+    print(df_r)
 
     # 指定した列を指定した値に置き換え
     df_cr = df_r.fillna({"人口（総数）": "hoge", "人口（男）": 0, "人口（女）": 0})
-    # print(df_cr)
+    print(df_cr)
 
     # 欠損値を0で補完
     df_0 = df_cr.fillna(0)
-    # print(df_0)
+    print(df_0)
     """"""
 
     """条件抽出"""
     df_tokyo = df_0[df_0[r"都道府県名"] == "東京都"]  # ブールインデックス
     df_tokyo = df_0.query("都道府県コード == '13'")
     df_kyoto = df_0[df_0["都道府県名"].isin(["京都府"])]
-    # print(df_kyoto)
+    print(df_kyoto)
     """"""
 
     """統計量算出"""
-
     # 基本統計量
-    # print(df_0.dtypes)
+    print(df_0.dtypes)
     df_men = df_0["人口（男）"].astype(float)
-    # print(df_men.describe())
-    # print(df_men.max())
-    # print(df_men.min())
-    # print(df_men.count())
-    # print(df_men.std())
-    # print(df_men.mean())
-    #
-    # # 相関
-    # df_women = df_0["人口（女）"].astype(int)
-    # print(df_men.corr(df_women))
+    print(df_men.describe())
+    print(df_men.max())
+    print(df_men.min())
+    print(df_men.count())
+    print(df_men.std())
+    print(df_men.mean())
+
+    # 相関
+    df_women = df_0["人口（女）"].astype(int)
+    print(df_men.corr(df_women))
     """"""
 
     """特徴抽出"""
-
     # カテゴリ化
     df_tokyo = df_tokyo.sort_values("西暦（年）")
 
@@ -96,17 +93,39 @@ if __name__ == '__main__':
     def era_to_binary(x):
         return 1 if x == "平成" else 0
 
+
     df_tokyo["元号"] = df_tokyo["元号"].apply(lambda e: 1 if e == "平成" else 0)
-    # print(df_tokyo)
+    print(df_tokyo)
 
     # 四則演算
-    # df_tokyo["男女比"] = df_men / df_tokyo["人口（女）"].astype(float)
-    # df_tokyo["平均差_男"] = df_men.mean() - df_men
-    # df_tokyo["log_men"] = np.log1p(df_men)
-    # print(df_tokyo)
+    df_tokyo["男女比"] = df_men / df_tokyo["人口（女）"].astype(float)
+    df_tokyo["平均差_男"] = df_men.mean() - df_men
+    df_tokyo["log_men"] = np.log1p(df_men)
+    print(df_tokyo)
 
     # one-hot-encoding
     print(pd.get_dummies(prefecture_code_series))
 
     # binning
+
+    # 最大値・最小値からのアプローチ
+    bin_quad, bins = pd.cut(df_tokyo["人口（男）"].astype(float), 4, retbins=True, labels=False)  # 最大値と最小値の間でn分割, 区間も同時に取得
+    print(bin_quad, bins)
+    bin_quad_label = pd.cut(df_tokyo["人口（男）"].astype(float), 4, retbins=True, labels=False)  # ラベリング化
+    print(bin_quad_label)
+    bin_quad_original_label = pd.cut(df_tokyo["人口（男）"].astype(float), 4, retbins=True,
+                                     labels=['S', 'M', 'L', 'XL'])  # ラベル付きの分割
+    print(bin_quad_original_label)
+
+    # 任意の境界でのアプローチ
+    twenty_first_century = pd.cut(df_tokyo["西暦（年）"].astype(float), [0, 2000, 2100], retbins=True)
+    print(twenty_first_century)
+
+    # 等分割
+    bin_quartile, bins = pd.qcut(df_tokyo["人口（男）"].astype(float), 4, retbins=True, labels=False,
+                                 duplicates='drop')  # 最大値と最小値の間でn分割, 区間も同時に取得
+    print(bin_quartile, bins)
     """"""
+
+    # 書き出し
+    df_tokyo.to_csv('tokyo.csv', header=True, index=False, encoding='utf-8')
--- refs.md
+++ refs.md
@@ -52,6 +52,77 @@ one-hot-encoding = one-hotなベクトルの特徴量エンジニアリング
     
 * デメリット
     * 大量のベクトルを生成することになるので、メモリ使用量・計算量が爆発的に増えていく
+    
+## binning
+binning = 連続値を境界を使って離散値に変換する処理
+
+例)
+* 20代, 30代, 40代
+
+## 応用
+メモリ問題
+
+基本的にはガベージコレクションでなんとかしてくれるが、実際にデータ分析をする際にメモリ使用量が気になる => 即時的にメモリ解放を行う必要あり
+
+* 読み込みを最低限にする
+```python
+import pandas as pd
+
+df_100_interval = pd.read_csv('input.csv', chunksize=100)  # 100行だけ読み込み
+next(df_100_interval)  # 100-200
+```
+
+* 読み込みを最適化する
+    * [Dask](https://docs.dask.org/en/latest/)
+    
+```python
+import dask.dataframe as dd
+
+df = dd.read_csv('*.csv')
+```
+
+* いらないものを即座に消す
+    * `del df`
+
+* ファイルにダンプする
+    * オーバーヘッドが欠点
+    
+*  破壊的代入
+
+```python
+big_data = [func(x) for x in big_data]
+
+for i, e in enumerate(big_data):
+    big_data[i] = func(e)
+
+```
+
+* 64bit型を使わない
+    * float32 (intも同様)
+        * float64よりも1/2のメモリ消費量で済む
+        * 一方正確な数値表現ではなくなる
+            * だが、大抵の機械学習では必要ない
+
+```python
+import numpy as np
+
+np.float32
+```
+
+* pandasの場合、デフォルトが64のためあらかじめ指定しておく必要がある
+
+```python
+import pandas as pd
+
+# example
+dtype = {'id': 'int16', 'date': 'int32', 'value': 'float32'}
+df = pd.read_csv('input.csv', dtype=dtype)
+
+df['value'] = df['value'].astype('float32')
+
+```
+
+メモリ溢れはないが、pythonでのfor文で配列/dataFrame処理はとても重くなりがち
 
 ## 特徴量生成
 機械学習寄り
